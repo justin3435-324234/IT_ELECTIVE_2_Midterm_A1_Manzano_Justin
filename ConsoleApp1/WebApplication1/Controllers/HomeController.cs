92b5c2fffb639d14ed37dd0cbd359894f051039f
justin3435-324234 <manzanojustin37@gmail.com>
2026-07-18 09:46:52 +0800

    Add files via upload

--- ConsoleApp1/WebApplication1/Controllers/HomeController.cs
+++ ConsoleApp1/WebApplication1/Controllers/HomeController.cs
@@ -11,6 +11,29 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
+
+    public IActionResult Skills()
+    {
+        return View();
+    }
+
+    public IActionResult Projects()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+
+
     public IActionResult Privacy()
     {
         return View();
